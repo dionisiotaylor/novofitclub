80ae6d86bc7a99bf4f4c32210ea9a2c2c8236b20
Dionisio <dio@blacksmith.agency>
2025-08-09 11:06:17 -0600

    changes on index for google

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,6 @@
 import '../styles/index.scss';
 import '../assets/main.css'
 
-
 export const metadata = {
   title: 'Novo Fit Club',
   description: 'Somos un gimnasio de acondicionamiento físico. Train like a beast!',
@@ -9,6 +8,10 @@ export const metadata = {
     icon: 'https://novofitclub.com/wp-content/uploads/2024/10/cropped-favicon-1-32x32.png',
     shortcut: 'https://novofitclub.com/wp-content/uploads/2024/10/cropped-favicon-1-192x192.png',
     apple: 'https://novofitclub.com/wp-content/uploads/2024/10/cropped-favicon-1-180x180.png',   
+  },
+  robots: 'noindex, nofollow',
+  verification: {
+    google: 'nm62yAjhSE1VBCQXw08RK41l9B-nOhQX6QuxWGAuC8c'
   }
 }
 
